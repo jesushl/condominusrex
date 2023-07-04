b0362bd1141023152f0f58f2ad95c604c847a85a
jesushl <jesushledon@gmail.com>
2023-07-04 00:33:42 -0600

    A simple list of common areas in a habitational area

--- common_areas/views.py
+++ common_areas/views.py
@@ -4,9 +4,15 @@ from .models import Common_Area, Person, Location, Habitational_Area, Home
 
 # Create your views here.
 def index(request):
-    common_areas = Common_Area.objects.order_by('id')[:10]
-    output = " ,".join([ca.name for ca in common_areas])
-    return HttpResponse(output)
+    habitational_area = Habitational_Area.objects.get(id=1)
+    common_areas = Common_Area.objects.filter(
+            habitational_area=habitational_area
+        ).order_by('id')[:10]
+    context = {
+        'habitational_area': habitational_area,
+        'common_areas': common_areas
+    }
+    return render(request,"common_areas/index.html" ,context=context)
 
 
 def common_area(request, common_area_id):
