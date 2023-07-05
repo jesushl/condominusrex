18976b1cf95a3135b7d5a4c66af9c13a42af100b
jesushl <jesushledon@gmail.com>
2023-07-04 20:45:01 -0600

    Adding new urls

--- common_areas/urls.py
+++ common_areas/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:common_area_id>", views.common_area, name="common_area")
+    path(
+        "<int:common_area_id>/common_area/",
+        view=views.common_area,
+        name="common_area"
+    ),
+    path(
+        "<int:habitational_area_id>/habitational_area/",
+        view=views.habitational_area,
+        name="habitational_area"
+    ),
 ]
