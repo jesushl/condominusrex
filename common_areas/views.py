a1bb25d6f666ac6d13180800fec573e2185c3d0f
jesushl <jesushledon@gmail.com>
2023-07-04 20:45:41 -0600

    Diferentiation between habitational areas and common aras

--- common_areas/views.py
+++ common_areas/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 from .models import Common_Area, Person, Location, Habitational_Area, Home
 
 # Create your views here.
 def index(request):
-    habitational_area = Habitational_Area.objects.get(id=1)
+    try:
+        habitational_area = Habitational_Area.objects.get(id=1)
+    except Habitational_Area.DoesNotExist as dne:
+        raise Http404("Habitational Area not exits")
     common_areas = Common_Area.objects.filter(
             habitational_area=habitational_area
         ).order_by('id')[:10]
@@ -15,6 +18,28 @@ def index(request):
     return render(request,"common_areas/index.html" ,context=context)
 
 
+def habitational_area(request, habitational_area_id):
+    try:
+        ha = Habitational_Area.objects.get(pk=habitational_area_id)
+        common_areas = Common_Area.objects.filter(habitational_area=ha)
+        context = {
+            'habitational_area': ha,
+            'common_areas': common_areas
+        }
+    except Habitational_Area.DoesNotExist as hane:
+        raise Http404("Habitational Area not exists")
+    return render(
+        request=request,
+        template_name="common_areas/index.html",
+        context=context
+    )
+
 def common_area(request, common_area_id):
-    _ = f"Ready to see a common area: {common_area_id}"
-    return HttpResponse(_)
+    common_area = get_object_or_404(Common_Area, pk=common_area_id)
+    print(common_area.name)
+    context = {'common_area': common_area}
+    return render(
+        request=request,
+        template_name="common_areas/common_area.html",
+        context=context
+    )
